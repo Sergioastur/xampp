85393fdcffbd6acea2b63e0ef3640d8ca0f1e680
Sergioastur <151532958+Sergioastur@users.noreply.github.com>
2024-10-02 19:42:09 +0200

    02-10-24 casa

--- Ejercicios/U2/Practica 2/ej6.php
+++ Ejercicios/U2/Practica 2/ej6.php
@@ -10,12 +10,20 @@
         array(0,0,0,0,0)
     );
 
-    
+    $filaColumna = array(0, 0);
+    $mayor = 0;
 
     for ($i = 0; $i<4; $i++) {
         for ($f = 0; $f<5; $f++) {
-            $M[$i][$f] = mt_rand(0, 10);
+            $M[$i][$f] = mt_rand(0, 99);
             
+            if ($M[$i][$f]>$mayor) {
+                $mayor = $M[$i][$f];
+                $filaColumna [0] = $i;
+                $filaColumna [1] = $f;
+            }
         }
     }
+
+    echo "El numero mayor de la matriz es ".$mayor."<br> Fila: ".$filaColumna[0]."<br> Columna: ".$filaColumna[1];
 ?>
