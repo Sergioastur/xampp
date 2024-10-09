5cbb421970ce9c1c9f3686e8b8f791e1ff069f51
Sergioastur <151532958+Sergioastur@users.noreply.github.com>
2024-10-09 18:45:26 +0200

    9-10-24

--- Ejercicios/U3/Uni3_Pra3 SergioSaavedra/ej5.php
+++ Ejercicios/U3/Uni3_Pra3 SergioSaavedra/ej5.php
@@ -3,46 +3,21 @@
     Escribe un script para probar algunas de las funciones mostradas debajo, el sript
     ha de contener al menos tres funciones de cada bloque
 */
-/*
-echo "<h1>Funciones de variables</h1> <br>";
-$var = "hola";
-echo isset($var)."<br>";
-$var = null;
-echo is_null($var)."<br>";
-$var = false;
-echo empty($var)."<br>";
 
-echo "<h1>Funciones de cadenas</h1> <br>";
-$cad = "1234";
-echo strlen($cad)."<br>";
-$cad = "Hola_q_tal";
-$token = "_";
-$array = explode($token, $cad);
-echo var_dump($array)."<br>";
-$txt = implode($token, $array);
-echo $txt."<br>";
-
-echo "<h1>Funciones de array</h1> <br>";
-$arr = array(5,3,2);
-sort($arr);
-echo var_dump($arr)."<br>";
-$arr = array("size" => "XL", "color" => "gold");
-$val = array_values($arr);
-echo var_dump($val)."<br>";
-echo  count($arr);
-*/
 
 $correo = "sergiosr96@educastur.es";
-$correo = strtolower($correo);
+
 if (is_string($correo) && !is_null($correo)) {
+    $correo = strtolower($correo);
     $datos = explode("@", $correo);
 
     if (!array_key_exists(1, $datos) || count($datos)>2) {
-        echo ("Correo no valido");
+        echo ("Correo no valido compruebe los datos <br>");
+        print_r(array_values($datos));
     } else {
         echo "Usuario: ".$datos[0]."<br> Proveedor de correo: ".$datos[1];
 
-        if (strcmp("educastur.es", $datos[1])) {
+        if (strcmp("educastur.\*", $datos[1])) {
             echo ("<br>Tiene un correo educativo");
         }
     }
