625f92f245599aa7b03ffa0216a20eb5addcf4d7
“Sergio <151532958+Sergioastur@users.noreply.github.com>
2024-10-28 14:10:59 +0100

    28-10-24

--- Ejercicios/U4/Uni4_Pra3 SergioSaavedra/ej1/upload.php
+++ Ejercicios/U4/Uni4_Pra3 SergioSaavedra/ej1/upload.php
@@ -5,7 +5,7 @@
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     // Comprobamos si es una imagen real o falsa
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
+    if($check !== false  && move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
     echo "El fichero es una imagen - " . $check["mime"] . ".";
     $uploadOk = 1;
     } else {
@@ -13,4 +13,21 @@
     $uploadOk = 0;
     }
     
+    // Comprobamos si el fichero ya existe
+    if (file_exists($target_file)) {
+        echo "El fichero existe.";
+        $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "El fichero es demasiado grande.";
+        $uploadOk = 0;
+    }
+
+    // Permitir solo determinados formatos
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
 ?>
