95a9378aab7f217f03eefc900506cc7b62e48f25
“Sergio <151532958+Sergioastur@users.noreply.github.com>
2024-11-27 14:17:47 +0100

    27-11-24

--- Ejercicios/U4/EJEMPLOS/DB/conexion.php
+++ Ejercicios/U4/EJEMPLOS/DB/conexion.php
@@ -2,21 +2,21 @@
 require_once 'login.php';
 $connection = new mysqli($hn, $un, $pw, $db);
 if ($connection->connect_error) die("Fatal Error");
-$query = "SELECT USU,CONTRA FROM usuarios";
+$query = "SELECT * FROM usuarios";
 $result = $connection->query($query);
 if (!$result) die("Fatal Error");
 $rows = $result->num_rows;
 for ($j = 0 ; $j < $rows ; ++$j)
 {
-/* $result->data_seek($j);
-echo 'ID: ' .htmlspecialchars($result->fetch_assoc()['ID']) .'<br>'; */
+$result->data_seek($j);
+echo 'ID: ' .htmlspecialchars($result->fetch_assoc()['ID']) .'<br>'; 
 $result->data_seek($j);
 echo 'USU: ' .htmlspecialchars($result->fetch_assoc()['USU']) .'<br>';
 $result->data_seek($j);
 echo 'CONTRA: ' .htmlspecialchars($result->fetch_assoc()['CONTRA']) .'<br>';
-/* $result->data_seek($j);
+$result->data_seek($j);
 echo 'ROL: '. htmlspecialchars($result->fetch_assoc()['ROL'])
-.'<br>'; */
+.'<br><br>';
 /* $result->data_seek($j);
 echo 'ISBN: ' .htmlspecialchars($result->fetch_assoc()['isbn'])
 .'<br><br>'; */
