19d63dc08974aab5906b2606a6edcff8e8b27280
“Sergio <151532958+Sergioastur@users.noreply.github.com>
2025-02-26 14:15:40 +0100

    26-02-25

--- Ejercicios/U8/VIDEOCLUBPRACTICA/resources/views/catalog/show.blade.php
+++ Ejercicios/U8/VIDEOCLUBPRACTICA/resources/views/catalog/show.blade.php
@@ -6,7 +6,32 @@
 </head>
 <body>
 @extends('layouts.master')
-@section('content')Vista detalle de película {{$id}}
+@section('content')<div class="row">
+<div class="col-sm-4">
+<img src="{{$arrayPeliculas[$id]['poster']}}" style="height:200px"/>
+</div>
+<div class="col-sm-8">
+{{-- TODO: Datos de la película --}}
+<h1>{{$arrayPeliculas[$id]['title']}}</h1>
+<h3>Año: {{$arrayPeliculas[$id]['year']}}</h3>
+<h4>Director: {{$arrayPeliculas[$id]['director']}}</h4>
+<p><strong>Resumen:</strong> {{$arrayPeliculas[$id]['synopsis']}}</p>
+<p><strong>Estado:</strong>
+@if($arrayPeliculas[$id]['rented'])
+Película actualmente alquilada
+<br>
+<a href="#" class="btn btn-danger">Devolver película</a>
+@else
+Película disponible
+<br>
+<a href="#" class="btn btn-primary">Alquilar película</a>
+@endif
+<a href="{{ url('/catalog/edit/' . $id ) }}" class="btn btn-warning">Editar película</a>
+<a href="{{ url('/catalog') }}" class="btn btn-outline-secondary">Volver al listado</a>
+</p>
+
+</div>
+</div>
 @stop
 </body>
 </html>
