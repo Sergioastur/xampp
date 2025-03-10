871bc948eac3a9846a69142d7ff03c7e119ecc7c
“Sergio <151532958+Sergioastur@users.noreply.github.com>
2025-03-10 14:07:18 +0100

    10-03-25

--- Ejercicios/U8/VIDEOCLUBPRACTICA/resources/views/catalog/create.blade.php
+++ Ejercicios/U8/VIDEOCLUBPRACTICA/resources/views/catalog/create.blade.php
@@ -6,7 +6,35 @@
 </head>
 <body>
 @extends('layouts.master')
-@section('content')Añadir película
+@section('content')<div class="row">
+<div class="col-sm-8">
+<h1>Nueva película</h1>
+<form action="" method="post">
+{{ csrf_field() }}
+<div class="form-group">
+<label for="title">Título</label>
+<input type="text" class="form-control" id="title" name="title">
+</div>
+<div class="form-group">
+<label for="year">Año</label>
+<input type="text" class="form-control" id="year" name="year">
+</div>
+<div class="form-group">
+<label for="director">Director</label>
+<input type="text" class="form-control" id="director" name="director">
+</div>
+<div class="form-group">
+<label for="poster">Poster</label>
+<input type="text" class="form-control" id="poster" name="poster">
+</div>
+<div class="form-group">
+<label for="synopsis">Resumen</label>
+<textarea class="form-control" id="synopsis" name="synopsis"></textarea>
+</div>
+<button type="submit" class="btn btn-primary">Crear película</button>
+</form>
+</div>
+</div>
 @stop
 </body>
 </html>
