b6b752d75ef79bd816d8131064a4efe6d9740dfb
Sergioastur <151532958+Sergioastur@users.noreply.github.com>
2025-03-20 19:53:38 +0100

    Update web.php

--- Ejercicios/U8/pictograma/routes/web.php
+++ Ejercicios/U8/pictograma/routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\AgendaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AgendaController::class, 'listado']);
 
 Route::get('agenda', [AgendaController::class, 'listado']);
 
